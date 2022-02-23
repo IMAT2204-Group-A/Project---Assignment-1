164e6d181c332a712c6bb74ae2ee6e2f8d3fc595
Michał Mazur <mazurek11331@gmail.com>
2022-02-23 11:34:47 +0000

    Classes finished.

---
+++ ClassLibraryCJMKLtd/clsDVDGenre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCJMKLtd
+{
+    public class clsDVDGenre
+    {
+        protected int dvdid;
+        protected int genreid;
+
+        public clsDVDGenre(int dvdID, int genreID)
+        {
+            this.dvdid = dvdID;
+            this.genreid = genreID;
+        }
+        public int DVDID
+        {
+            get { return dvdid; }
+            set { dvdid = value; }
+        }
+    }
+}
